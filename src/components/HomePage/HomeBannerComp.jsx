a2006f7131a329d696db9fa20dc8e9736add2cc2
agusdwis <agusdwi@alterra.id>
2020-06-08 12:11:28 +0700

    update

--- src/components/HomePage/HomeBannerComp.jsx
+++ src/components/HomePage/HomeBannerComp.jsx
@@ -13,7 +13,7 @@ const HomeBanner = () => {
                      style={{backgroundImage: "url("+ Background+")"}}>
                     <div className="text-white text-center">
                         <div>
-                            <h2 className="card-title h1-responsive pt-3 mb-5 font-bold">Cheapest Online Bookstore on Earth</h2>
+                            <h2 className="card-title h1-responsive pt-3 mb-5 font-bold">Cheapest Online Bookstore on Earth!!</h2>
                             <Link to="/book" className="btn btn-lg btn-success purple-gradient btn-rounded mb-4"><i className="fas fa-book left"/> Go Shop</Link>
                         </div>
                     </div>
